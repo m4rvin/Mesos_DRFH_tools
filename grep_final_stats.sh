28284f167b44af8a06bc49d4cad1a30bcf54382c
Daniele Angelucci <angelucci.dnl@gmail.com>
2017-03-17 10:27:05 +0100

    Added script to grep final stats from .dat files.

---
+++ grep_final_stats.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ "$#" -lt 1 ]; then
+  	echo ">>> WARNING: You must specify the number of frameworks' files to read stats from."
+	exit
+fi
+
+if [ "$#" -lt 2 ]; then
+  	echo "->Looking in the current folder for stats files."
+	folder="./"
+else
+	echo "->Looking in the $2 folder for stats files."
+	folder=$2
+fi
+
+
+numFrameworks=$1
+
+
+echo
+i=1
+while [ $i -le $numFrameworks ]; do
+	echo ">>Stats for framework-$i.dat"
+	IFS=' '  read -r ARN RO OA OD OU TOD TOA TOU TOR  TL TNL TTL TTNL  < <(tail -n1 "$folder/framework-$i.dat") && echo "- allocationRunNumber = $ARN" && echo "- totalOffersDeclined = $TOD"
+	let i=i+1	
+	echo
+done
